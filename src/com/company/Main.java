ae3c4488b8903e08056ca1932b89cfdb2d0a9c47
jamieday22 <jamierobertday@hotmail.com>
2022-01-23 21:13:12 +0000

    multiply and add added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+
+
+
     }
-        public static void walls () {
+
+
+
+
+
+
+        public static int[] walls () {
             System.out.println("please enter the number of walls you want to paint");
             Scanner scanner = new Scanner(System.in);
 
@@ -21,9 +31,9 @@ public class Main {
 
 
             for (int i = 0; i < numberOfWalls; i++) {
-                System.out.println("please input the height of all the wall ");
+                System.out.println("please input the height of the wall ");
                 height[i] = scanner.nextInt();
-                System.out.println("now please print the width");
+                System.out.println("now input print the width");
                 width[i] = scanner.nextInt();
 
                 System.out.println();
@@ -36,7 +46,71 @@ public class Main {
 
             System.out.println("The area for each of the walls from 1 to " + numberOfWalls + " is " + Arrays.toString(result));
 
+            System.out.println("=======================================================================");
+
+            return result;
         }
+
+        public static void doors () {
+        System.out.println("please enter the number of doors you have inside the walls you want to paint ");
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfDoors = scanner.nextInt();
+        int[] height = new int[numberOfDoors];
+        int[] width = new int[numberOfDoors];
+
+        //creating an array from the number the user puts in
+
+
+        for (int i = 0; i < numberOfDoors; i++) {
+            System.out.println("please input the height of the door ");
+            height[i] = scanner.nextInt();
+            System.out.println("now please input the width");
+            width[i] = scanner.nextInt();
+
+            System.out.println();
+            System.out.println("the height and width of the door is " + height[i] * width[i]);
+            System.out.println();
+        }
+        int[] result = times(height, width);
+
+        System.out.println("the height of doors from 1 to " + numberOfDoors + " is " + Arrays.toString(height) + "" + " the width of doors from 1 to " + numberOfDoors + " is " + Arrays.toString(width));
+
+        System.out.println("The area for each of the doors from 1 to " + numberOfDoors + " is " + Arrays.toString(result));
+
+        System.out.println("=======================================================================");
+    }
+
+        public static void windows () {
+        System.out.println("please enter the number of windows you have inside the walls you want to paint ");
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfWindows = scanner.nextInt();
+        int[] height = new int[numberOfWindows];
+        int[] width = new int[numberOfWindows];
+
+        //creating an array from the number the user puts in
+
+
+        for (int i = 0; i < numberOfWindows; i++) {
+            System.out.println("please input the height of the window ");
+            height[i] = scanner.nextInt();
+            System.out.println("now please input the width");
+            width[i] = scanner.nextInt();
+
+            System.out.println();
+            System.out.println("the height and width of the door is " + height[i] * width[i]);
+            System.out.println();
+        }
+        int[] result = times(height, width);
+
+        System.out.println("the height of window from 1 to " + numberOfWindows + " is " + Arrays.toString(height) + "" + " the width of window from 1 to " + numberOfWindows + " is " + Arrays.toString(width));
+
+        System.out.println("The area for each of the window from 1 to " + numberOfWindows + " is " + Arrays.toString(result));
+
+        System.out.println("=======================================================================");
+    }
+
 
 
         //this multiplies 2 arrays, starts with [0]*[0] then the for loop will add 1 to each and go again
@@ -50,4 +124,15 @@ public class Main {
 
             return result;
         }
+
+         public static int[] add(int[] first, int [] second) {
+            int length = Math.min(first.length, second.length);
+            int[] result = new int[length];
+
+            for (int i = 0; i < length; i++) {
+                result[i] = first[i] * second[i];
+        }
+
+            return result;
+    }
 }
